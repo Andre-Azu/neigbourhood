560ee5407c54951c0235eea68a8279b462430bc6
andre azu <andreazuaa@gmail.com>
2022-01-12 23:57:17 +0300

    fix bug

--- nhood_app/urls.py
+++ nhood_app/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('',views.welcome, name='welcome'),
-    path('',views.welcome, name='welcome'),
+    path('',views.welcome, name='welcome'),    
+    path('login/', views.login_view, name='login'),
     path('signup/', views.usersignup, name='signup'),
     path('activate/<uidb64>/<token>/',views.activate_account, name='activate'),
     path('registration/', include('django_registration.backends.activation.urls')),
     path('registration/', include('django.contrib.auth.urls')),
     
 ] 
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
