326b17d2c0ac2da8638eaaa1f727e314168e21ef
andre azu <andreazuaa@gmail.com>
2022-01-12 23:58:51 +0300

    add user change form

--- nhood_app/forms.py
+++ nhood_app/forms.py
@@ -1,5 +1,5 @@
 from django.forms import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
